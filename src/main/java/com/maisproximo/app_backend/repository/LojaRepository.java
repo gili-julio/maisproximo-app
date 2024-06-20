83649add93bc65584e78f234e09f3d579714cac4
gili-julio <giliardoplay@gmail.com>
2024-06-19 21:50:22 -0300

    Repositorio de lojas criado

---
+++ src/main/java/com/maisproximo/app_backend/repository/LojaRepository.java
@@ -0,0 +1,7 @@
+package com.maisproximo.app_backend.repository;
+
+import com.maisproximo.app_backend.entity.Loja;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LojaRepository extends JpaRepository<Loja, Long> {
+}
