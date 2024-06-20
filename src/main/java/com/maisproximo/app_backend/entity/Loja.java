a65b74426806002b560eefb08162d562ea786677
gili-julio <giliardoplay@gmail.com>
2024-06-19 21:38:49 -0300

    Entidade loja criada

---
+++ src/main/java/com/maisproximo/app_backend/entity/Loja.java
@@ -0,0 +1,28 @@
+package com.maisproximo.app_backend.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "lojas")
+public class Loja {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false, length = 60)
+    private String nome;
+
+    @Column(name = "cnpj_or_cpf", unique = true, nullable = false, length = 14)
+    private Long cnpjOrCpf;
+
+}
